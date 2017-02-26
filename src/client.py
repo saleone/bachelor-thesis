79c37981647dbb558307a306aaf4f9f33f21e290
Saša Savić <sasa@savic.pw>
2017-02-26 21:50:55 +0100

    Use integers as data from client

--- src/client.py
+++ src/client.py
@@ -3,27 +3,29 @@ import socket
 import time
 import math
 import random
+import sys
 
 HOST, PORT = 'localhost', 7777
 
-LIMIT = 0.5
+if len(sys.argv) > 1:
+    PORT = int(sys.argv[1])
 
-posx = random.uniform(-50.00, 50.00)
-posy = random.uniform(-50.00, 50.00)
-posz = random.uniform(-50.00, 50.00)
+posx = random.randint(-100, 100)
+posy = random.randint(-100, 100)
+posz = random.randint(-100, 100)
 
 def change_pos(*values):
-    range_delta = 0.1
+    range_delta = 10
 
     output = []
     for pos in values:
         pos_min = pos - range_delta
-        pos_min = -0.5 if pos_min < -0.5 else pos_min
+        pos_min = -100 if pos_min < -100 else pos_min
 
         pos_max = pos + range_delta
-        pos_max = 0.5 if pos_max > 0.5 else pos_max
+        pos_max = 100 if pos_max > 100 else pos_max
 
-        output.append(round(random.uniform(pos_min, pos_max), 2))
+        output.append(random.randint(pos_min, pos_max))
 
     return output
 
@@ -33,7 +35,7 @@ while True:
         sock.connect((HOST, PORT))
         data = ';'.join([str(posx), str(posy), str(posz)])
         sock.sendall(bytes(data, 'utf-8'))
-        time.sleep(0.5)
+        time.sleep(1)
         posx, posy, posz = change_pos(posx, posy, posz)
     num += 1
 
