5f8da3c286bf734302ee493e00675b84836ba10e
Saša Savić <sasa@savic.pw>
2017-02-26 21:50:07 +0100

    Use other ports and show inv problem solutions

--- src/server.py
+++ src/server.py
@@ -3,17 +3,30 @@ import socketserver
 from socketserver import BaseRequestHandler
 from socketserver import TCPServer
 
-HOST = ''
-PORT = 7777
+from inv_kinematics import get_angles
 
 class ConnectionHandler(BaseRequestHandler):
 
     def handle(self):
-        self.data = self.request.recv(128).strip()
-        print('{} wrote:\n{}'.format(self.client_address[0], self.data))
+        self.data = self.request.recv(128).strip().decode()
+        print('Data from {}:\n\t {}'.format(self.client_address[0], self.data))
+        x, y, z = [int(value) for value in self.data.split(';')]
+        theta0, theta1, theta2 = get_angles(x, y, z)
+        print('\n')
+        print('Setting angles to: \n\t{};{};{}'.format(theta0, theta1, theta2))
+        print('\n{}\n'.format('='*40))
 
 def main():
-    server = TCPServer((HOST, PORT), ConnectionHandler)
+    host = ''
+    port = 7777
+    server = None
+    while server is None:
+        try:
+            server = TCPServer((host, port), ConnectionHandler)
+        except OSError:
+            port += 1
+            continue
+    print("Serving on: {}".format(port))
     server.serve_forever()
     server.server_close()
 
